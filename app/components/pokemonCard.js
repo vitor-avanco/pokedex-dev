0544ad5dd666442988ed620a0f89c38a414892e3
Vitor Avanco <vitor.avanco@hotmail.com>
2018-03-01 18:50:36 -0300

    pokemon-card component

---
+++ app/components/pokemonCard.js
@@ -0,0 +1,15 @@
+angular.module('pokedex').component('pokemonCard',{
+    bindings: {
+        pokemonid: '@', 
+        name: '@'
+    },
+    template: `
+        <div class="card">
+            <img class="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{{$ctrl.pokemonid}}.png" alt="{{$ctrl.name}}">
+            <div class="card-body">
+            <h5 class="card-title">#{{ $ctrl.pokemonid}} {{$ctrl.name}}</h5>
+            <a href="#" class="btn btn-primary">Mais detalhes</a>
+            </div>
+        </div>
+    ` 
+})
